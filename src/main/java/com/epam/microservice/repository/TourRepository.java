9fb1ad9261b53b22e9ef8bcc63e462add1bc9cd5
Stanislau_Sukora <stanislau.sukora@ukg.com>
2022-06-20 21:40:00 +0300

    dto based API

--- src/main/java/com/epam/microservice/repository/TourRepository.java
+++ src/main/java/com/epam/microservice/repository/TourRepository.java
@@ -4,7 +4,6 @@ import com.epam.microservice.domain.Tour;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
