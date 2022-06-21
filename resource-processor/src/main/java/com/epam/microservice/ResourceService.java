a876aa506492afb46ebacfa0465513a7574e42a5
Stanislau_Sukora <stanislau.sukora@ukg.com>
2022-06-21 13:43:15 +0300

    init services

---
+++ resource-processor/src/main/java/com/epam/microservice/ResourceService.java
@@ -0,0 +1,12 @@
+package com.epam.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceService.class, args);
+    }
+}
